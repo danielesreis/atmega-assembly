32a40f672a055412eef71bf3d55dd9f63277958f
Daniele Reis <danielesreis@outlook.com>
2018-02-24 02:25:16 -0300

    arithmetic progression added

--- ex3.asm
+++ ex3.asm
@@ -1,23 +1,9 @@
-;storing 0 to memory addresses from 0x100 to 0x120 
+ geometric progression of 2
 
-;inicializo Y com o endereço 0100
-ldi r28, $0
-ldi r29, $1
+ldi r16, 1
+ldi r17, 2
 
-start:
-;carrego no endereço 0100 o que está contido em r26
-;sts $100, r26
-
-;carrego nos registradores o que está contido em Y
-;ld r25, Y+
-;ld r24, Y+
-;ld r23, Y
-
-;armazeno no endereço apontado por Y o que está em r26
-st Y, r26
-inc r28
-
-cpi r28, 33
-brne start
-
-nop
+loop:
+mul r16, r17
+mov r16, r0
+rjmp loop
---
+++ ex4.asm
@@ -0,0 +1,8 @@
+; arithmetic progression of 2
+
+clr r16
+ldi r17, 2
+
+loop:
+add r16, r17
+rjmp loop
