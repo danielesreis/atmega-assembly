a945ceb0c6f7b095a1e3a0dee9cf94cc900582c4
danielesreis <danielesreis@bitbucket.org>
2018-03-20 02:49:06 -0300

    timer0 finished I guess

---
+++ timer0-ctc.asm
@@ -0,0 +1,51 @@
+ldi r16, 64
+out DDRD, r16
+
+clr r16
+out PORTD, r16
+out TCNT0, r16
+
+ldi r16, 42
+out TCCR0A, r16
+
+ldi r16, 5
+out TCCR0B, r16
+
+ldi r22, 255
+out OCR0A, r22
+
+ldi r16, 7
+out TIFR0, r16
+
+ldi r19, 0x40
+
+loop:
+
+no_overflow:
+sbis TIFR0, 1
+rjmp no_overflow
+
+ldi r16, 2
+out TIFR0, r16
+
+in r16, OCR0A
+dec r16
+cpi r16, 0
+breq set_255
+
+continue:
+out OCR0A, r16
+
+rjmp toggle
+
+set_255:
+mov r16, r22
+rjmp continue
+
+toggle:
+mov r20, r19
+in r21, PORTD
+eor r19, r21
+out PORTD, r19
+mov r19, r21
+rjmp loop
---
+++ timer0-fast-pwm.asm
@@ -0,0 +1,48 @@
+ldi r16, 0x40
+out DDRD, r16
+
+ldi r16, 0x07
+out TIFR0, r16 
+
+ldi r21, 0x40
+ldi r22, 0x02
+
+clr r16
+out PORTD, r16
+out TCNT0, r16
+out OCR0A, r16
+
+ldi r16, 0x83
+out TCCR0A, r16
+
+ldi r16, 0x05
+out TCCR0B, r16
+
+loop:
+
+sbic TIFR0, 1
+call setup
+rjmp loop	
+
+setup:
+out TIFR0, r22
+in r23, OCR0A
+inc r23
+cpi r23, 255
+breq mod
+
+continue:
+out OCR0A, r23
+
+toggle:
+in r17, PORTD
+mov r20, r21
+
+eor r21, r17
+out PORTD, r21
+mov r21, r20
+ret
+
+mod:
+ldi r23, 0
+rjmp continue
